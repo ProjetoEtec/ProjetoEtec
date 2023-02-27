7c209f47258e9db6d454e0955d9cb0f9fcbfc9dc
José Fernando <jnandolima88@gmail.com>
2023-02-27 00:02:20 -0300

    aceitar json do front

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const path = require('path');
 const app = express();
 const cliente = require('./routes/cliente')
 
+
+
 //Configurações
     //Template engine
     app.set('view engine', 'ejs');
     //Public
     app.use(express.static(path.join(__dirname, 'public')));
+    //body parser
+    app.use(express.urlencoded({extended:false}))
+    app.use(express.json())
 
 // rotas
 app.use('/cliente',cliente)
