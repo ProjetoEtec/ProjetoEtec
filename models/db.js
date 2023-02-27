891cf29cc186c695ab29c02162ebe0b383936445
José Fernando <jnandolima88@gmail.com>
2023-02-27 00:03:23 -0300

    conexão com banco de dados

---
+++ models/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const Sequelize = require('sequelize')
+const sequelize =  new Sequelize('bd_projeto_etec',process.env.DATABASE_USERNAME,process.env.DATABASE_PASSWORD,{
+  host:process.env.DATABASE_HOST,
+  dialect:"mysql",
+  dialectOptions:{
+    ssl:{
+      rejectUnauthorized: true
+    }
+  }
+})
+console.log('Connected to PlanetScale!')
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+module.exports = {
+  Sequelize:Sequelize,
+  sequelize:sequelize
+}
