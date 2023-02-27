5d8239c2fcbf5f9fab5c6e469ea86e0c45e55067
José Fernando <jnandolima88@gmail.com>
2023-02-27 00:04:22 -0300

    rota post e cadastro de cliente

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Cliente = require('../models/cliente')
 
 router.get('/',(req,res)=>{
   res.send('teste')
@@ -8,10 +9,24 @@ router.get('/',(req,res)=>{
 router.get('/cadastro', (req, res) =>{
   res.render('cliente/cadastro');
 })
+
+router.post('/cadastro/add',(req,res)=>{
+  Cliente.create({
+    nome: req.body.nomeCompleto,
+    cpf: req.body.cpf,
+    telefone: req.body.telefone,
+    data_nasc: req.body.dataNasc
+  }).then(()=>{
+    res.send("usuario criado com sucesso")
+  }).catch((err)=>{
+    res.send("Houve um erro ao criar o usuario")
+  })
+})
+
 router.get('/login', (req, res) =>{
   res.render('cliente/login');
 });
-router.get('/login/update/:id', (req, res) => {
+router.get('/update/:id', (req, res) => {
   res.render('cliente/minhaconta');
 })
 
