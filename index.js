06f1ad666ba099527e66d7dc68ef940a4fd64e07
José Fernando <jnandolima88@gmail.com>
2023-03-02 14:20:32 -0300

    setando a pagina de views

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cliente = require('./routes/cliente')
 //Configurações
     //Template engine
     app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname,'views'))
     //Public
     app.use(express.static(path.join(__dirname, 'public')));
     //body parser
