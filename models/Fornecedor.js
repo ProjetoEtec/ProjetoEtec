74b9d695a9960cbe8bbf0ddf5234763ef5ccc15b
José Fernando <jnandolima88@gmail.com>
2023-04-01 18:23:42 -0300

    definindo model de fornecedor

--- models/Fornecedor.js
+++ models/Fornecedor.js
@@ -1,9 +1,30 @@
 const db = require('./db')
 
 const Fornecedor = db.sequelize.define("fornecedores", {
-  nome:{
+  razao_social:{
     type:db.Sequelize.STRING
-  }
+  },
+  nome_fantasia:{
+    type:db.Sequelize.STRING
+  },
+  cnpj:{
+    type:db.Sequelize.STRING
+  },
+  telefone:{
+    type:db.Sequelize.STRING
+  },
+  telefone:{
+    type:db.Sequelize.STRING
+  },
+  data_nascimento:{
+    type:db.Sequelize.STRING
+  },
+  email:{
+    type:db.Sequelize.STRING
+  },
+  senha:{
+    type:db.Sequelize.STRING
+  },
 })
 // criar id automático
 //Fornecedor.sync({force:true})
