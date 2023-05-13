48d95fa0fd372c0d9305d4f272077f4d841a3daa
José Fernando <jnandolima88@gmail.com>
2023-05-13 00:37:58 -0300

    git ignore

---
+++ public/js/cookies.js
@@ -0,0 +1,12 @@
+// // console.log("aa")
+
+// function addCart(a,b){
+//   var itemID = document.querySelector(a)
+//   var itemQTD = document.querySelector(b)
+//   console.log("ID do item"+itemID.textContent)
+//   console.log("QTD do item"+itemQTD.value)
+// }
+
+// Cookies.set('cookie_name', 'cookie_value',{
+//   expires: 7
+// });
